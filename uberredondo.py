cf802ba22dd2884a886073cc1201caff7d0cfc99
monseram0110-lab <monseram0110@gmail.com>
2025-12-19 13:01:03 -0600

    este es mi viajeredondo monse

---
+++ uberredondo.py
@@ -0,0 +1,12 @@
+print("  VIAJE EN AUTOBUS REDONDO ")
+print("____________________________________________")
+print("| DESTINO 1: Baja California  | COSTO: $550  |")
+print("| DESTINO 2: QUERETARO        | COSTO: $350  |")
+print("|__________________________________________|")
+d=input("Numero de destino al que se viajara: ")
+p=input("Numero de pasajeros que subiran: ")
+d1=550
+d2=350
+tp= d*p
+print("|SU TOTAL A PAGAR ES DE",tp,"|")
+print("|        FELIZ VIAJE         |")
